4f511b51b3036eedee1a9ce62f0cd9c2f3465894
TehilaCH <tehila111222@gmail.com>
2023-12-26 20:51:07 +0200

    Initial Code + Welcome functions

---
+++ Stage0/Program5956.cs
@@ -0,0 +1,18 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome5956();
+        Welcome7549();
+        Console.ReadKey();
+    }
+
+    static partial void Welcome7549();
+    private static void Welcome5956()
+    {
+        Console.WriteLine("Hello, World!");
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application", name);
+    }
+}
