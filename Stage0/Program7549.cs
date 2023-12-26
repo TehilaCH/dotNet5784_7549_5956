ee6248a08b7471140459a9de80c3d73dd4acbfea
Hodya Ch <chahodaya1@gmail.com>
2023-12-26 22:52:37 +0200

    Initial Code + Welcome functions

---
+++ Stage0/Program7549.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage0
+{
+    internal partial class Program
+    {
+    
+        private static void Welcome7549()
+        {
+            Console.WriteLine("I am also here!");
+        }
+    }
+}
