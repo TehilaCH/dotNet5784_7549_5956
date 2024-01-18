dc0f28ebb068849fc7b64d87b7672da4d7f064e3
TehilaCH <tehila111222@gmail.com>
2024-01-18 14:57:12 +0200

    פרק 1 - יצירת פרויקט חדש בשם DalXml פרק 2 - יצירת קבצי קונפיגורציה וקבצים שיהוו את בסיס נתונים - בתוך ספריה בשם xml פרק 3 - יצירת מחלקת Config עבור נתוני קונפיגורציה כלליים פרק 4- הוספת מחלקת עזר לעבודה עם קבצי XML בשם XmlTools

--- DalFacade/DalApi/ICrud.cs
+++ DalFacade/DalApi/ICrud.cs
@@ -9,7 +9,7 @@ public interface ICrud<T> where T : class
     int Create(T item); //Creates new entity object in DAL
     T? Read(Func<T, bool> filter);//method that returns an object not only by ID but by another parameter.
     T? Read(int id); //Reads entity object by its ID 
-    IEnumerable<T?> ReadAll(Func<T, bool>? filter = null);//Reads all entity objects
+    IEnumerable<T?> ReadAll(Func<T, bool>? filter = null);//Reads all entity objects 
     void Update(T item); //Updates entity object
     void Delete(int id); //Deletes an object by its Id
 
